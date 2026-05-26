73d5dc4cc581a3f9cc39c84b335919e69348465a
shravyashah <shravya.shah066@gmail.com>
2026-05-26 12:33:59 -0600

    Added clean_price function to remove dollarsigns and commas to represent integer for calculations later on

---
+++ src/clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def clean_price(df): # cleaning the price column
+    df["price"] = df["price"].astype(str)
+    df["price"] = df["price"].str.replace("$", "",regex=False)
+    df["price"] = df["price"].str.replace(",", "",regex=False)
+    df["price"] = pd.to_numeric(df["price"], errors='coerce')
+    df["price"] = df["price"].fillna(0)
+    df["price"] = df["price"].astype(int)
+    return df
--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import pandas as pd
+from clean import clean_price
 
 def main():
 
-    df = pd.read_csv("data/raw/data_ab.csv", low_memory = False)
+    df = pd.read_csv("data/raw/calgary_houses_raw.csv", low_memory = False)
     
-    keep_cols = ["addressLocality", "addressRegion", "price", "property-beds", "property-baths"]
-    df_new = df[keep_cols]
-
-
-    print(df_new.head(10))
-    print(df_new.info())
-    print(df_new.isna().sum())
-
+    #keep_cols = ["address","bedrooms","bathrooms","price","sqft","lot_size", "garage","community","property_type","days_on_markert"]
+    #df_new = df[keep_cols]
+    
+    df = clean_price(df)
+    print(df.head())
+    print(df["price"])
+    
+    
+    
 if __name__ == "__main__":
     main()
