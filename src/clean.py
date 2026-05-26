d2137d08a29ca2230cf339cbac06fdf273a4a0e0
shravyashah <shravya.shah066@gmail.com>
2026-05-26 12:48:51 -0600

    remove duplicates but need to do more cleaning for address

--- src/clean.py
+++ src/clean.py
@@ -8,3 +8,7 @@ def clean_price(df): # cleaning the price column
     df["price"] = df["price"].fillna(0)
     df["price"] = df["price"].astype(int)
     return df
+
+def clean_address(df): # cleaning the address column
+    df["address"] = df["address"].drop_duplicates()
+    return df
--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 import pandas as pd
-from clean import clean_price
+from clean import clean_price,clean_address
+
 
 def main():
 
     df = pd.read_csv("data/raw/calgary_houses_raw.csv", low_memory = False)
     
-    #keep_cols = ["address","bedrooms","bathrooms","price","sqft","lot_size", "garage","community","property_type","days_on_markert"]
-    #df_new = df[keep_cols]
+    keep_cols = ["address","bedrooms","bathrooms","price","sqft","lot_size", "garage","community","property_type","days_on_market"]
+    df_new = df[keep_cols]
     
-    df = clean_price(df)
-    print(df.head())
-    print(df["price"])
-    
+    df_new = clean_price(df_new)
+    df_new = clean_address(df_new)
+    print(df_new.head())
+    print(df_new["price"])
+    print(df_new["address"])
     
     
 if __name__ == "__main__":
