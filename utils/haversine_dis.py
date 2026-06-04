958d82b972e947389591250418f93936f0c828a5
shravyashah <shravya.shah066@gmail.com>
2026-06-04 12:11:40 -0600

    new file haversine distance and fixing poi currently

---
+++ utils/haversine_dis.py
@@ -0,0 +1,12 @@
+import math
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    radius = 6371  # Earth's radius in kilometers
+
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a)) * radius
+
+    return c
