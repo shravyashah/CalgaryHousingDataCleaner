8a24c12d3d39aa046d919a5040c537a65fd6a11f
shravyashah <shravya.shah066@gmail.com>
2026-06-05 15:11:16 -0600

    fixed some rounding and formatting

--- utils/haversine_dis.py
+++ utils/haversine_dis.py
@@ -8,6 +8,8 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     dlon = math.radians(lon2 - lon1)
 
     a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
-    c = 2 * math.asin(math.sqrt(a))
+    c = 2 * math.asin(math.sqrt(a)) 
+    result = c * radius
+    result = round(result, 2)  # Round the final distance to 2 decimal places
 
-    return c*radius
+    return result
